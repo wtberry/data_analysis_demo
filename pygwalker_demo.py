2545786bc931e640d6e33cc85bdb40829834df82
wataru takahashi <wtakahashi@watarunoMacBook-Pro.local>
2024-10-25 10:56:58 +0900

    feature: first commit, started running streamlit app, file uploader and analysis with pygwalker

---
+++ pygwalker_demo.py
@@ -0,0 +1,20 @@
+from pygwalker.api.streamlit import StreamlitRenderer
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    page_title="Pygwalker Demo",
+    page_icon="🐍",
+    layout="wide",
+)
+
+# uploaded file
+uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+
+if uploaded_file is not None:
+    # import data
+    df = pd.read_csv(uploaded_file)
+
+
+    pyg_app = StreamlitRenderer(df)
+    pyg_app.explorer()
