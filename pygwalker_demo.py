894100587733349cdcfdebff900b074a25a0a078
wataru takahashi <wtakahashi@watarunoMacBook-Pro.local>
2024-11-12 19:33:18 +0900

    feat: chat capacity

--- pygwalker_demo.py
+++ pygwalker_demo.py
@@ -3,12 +3,16 @@ from pathlib import Path
 
 from pygwalker.api.streamlit import StreamlitRenderer
 import pandas as pd
+from pandasai.llm import OpenAI, AzureOpenAI
+from pandasai import SmartDataframe
+from pandasai.responses.streamlit_response import StreamlitResponse
 import streamlit as st
 
 st.set_page_config(
-    page_title="Pygwalker Demo",
+    page_title="Data Analysis Dashboard Demo",
     page_icon="🐍",
     layout="wide",
+    initial_sidebar_state="expanded",
 )
 
 
@@ -19,28 +23,133 @@ with open(toml_path, "r") as f:
 
 encoding_options = config["data"]["encoding_options"]
 encoding_default = config["data"]["encoding_default"]
+agent_provider_options = config["chat"]["providers"]
 
-st.title("Data Analysis Dashboard Demo")
+st.title("Explore and Chat with your data")
+
+st.markdown("From the left sidebar, you can upload data file, select LLM provider, and chat with your data.")
+
+df = None
+llm_key = None
+agent = None
+azure_openai_endpoint_url = None
+azure_openai_api_version = None
+azure_openai_deployment_name = None 
+
 
 # uploaded file
-uploaded_file = st.file_uploader("Upload a csv or excel (xlsx) file", type=["csv", "xlsx"])
-encoding = st.selectbox(
-    label="Select Encoding for the file",
-    options=encoding_options
-)
-if uploaded_file is not None:
+with st.sidebar:
+    st.header("Settings")
+    st.markdown("## File Upload")
+    st.markdown("### Upload own data file")
+    uploaded_file = st.file_uploader("Upload a csv or excel (xlsx) file", type=["csv", "xlsx"])
+    encoding = st.selectbox(
+        label="Select Encoding for the file",
+        options=encoding_options
+    )
+    
+    st.markdown("### Download Sample Data")
+    download_file = pd.read_csv("data/titanic.csv").to_csv(index=False)
+    st.download_button("Download Sample Data", download_file, file_name="titanic.csv", mime="text/csv")
+
+    # chat agent settings
+    st.markdown("## Chat Agent Settings")
+    llm_provider = st.selectbox(
+        label="Select LLM Provider",
+        options=agent_provider_options,
+    )
+    llm_key = st.text_input(
+        label="Enter your LLM API key",
+        placeholder="Enter your LLM API key",
+        type="password",
+        value=None,
+    )
+    if llm_provider == "azure":
+        azure_openai_endpoint_url = st.text_input(
+            label="Enter your Azure OpenAI Endpoint URL",
+            placeholder="Enter your Azure OpenAI Endpoint URL",
+            type="password",
+            value=None,
+        )
+        azure_openai_api_version = st.text_input(
+            label="Enter your Azure OpenAI API Version",
+            placeholder="Enter your Azure OpenAI API Version",
+            value=None,
+        )
+        azure_openai_deployment_name = st.text_input(
+            label="Enter your Azure OpenAI Deployment Name",
+            placeholder="Enter your Azure OpenAI Deployment Name",
+            value=None,
+        )
+
+st.markdown("### Data Table")
+if uploaded_file is None:
+    st.write("Data table will be displayed here.")
+
+elif uploaded_file is not None:
     try:
-        # import data
-        if uploaded_file.name.endswith(".csv"):
-            df = pd.read_csv(uploaded_file, encoding=encoding)  
-        elif uploaded_file.name.endswith(".xlsx"):
-            df = pd.read_excel(uploaded_file, encoding=encoding)
-        else:
-            pass
-            # wrong file type should be already handled in st.file_uploader
+        with st.spinner("Loading data..."):
+            # import data
+            if uploaded_file.name.endswith(".csv"):
+                df = pd.read_csv(uploaded_file, encoding=encoding)  
+            elif uploaded_file.name.endswith(".xlsx"):
+                df = pd.read_excel(uploaded_file)
+            else:
+                pass
+                # wrong file type should be already handled in st.file_uploader
     except UnicodeDecodeError:
-        st.error(f"The file is not encoded in {encoding}. Please select a different encoding.")
+        st.sidebar.error(f"The file is not encoded in {encoding}. Please select a different encoding.")
     else:
+        st.sidebar.success("Data imported successfully.")
+        st.dataframe(df)
+
+    if llm_key is not None:
+        # initialize chat agent
+        if llm_provider == "openai":
+            llm = OpenAI(api_token=llm_key)
+            agent = SmartDataframe(df, config={"llm": llm, "verbose":True, "response_parser": StreamlitResponse})
+            st.sidebar.success("Chat agent initialized successfully.")
+        elif llm_provider == "azure":
+            if all([azure_openai_endpoint_url, azure_openai_api_version, azure_openai_deployment_name]):
+                llm = AzureOpenAI(
+                    api_token=llm_key,
+                    azure_endpoint=azure_openai_endpoint_url,
+                    api_version=azure_openai_api_version,
+                    deployment_name=azure_openai_deployment_name,
+                )
+                agent = SmartDataframe(df, config={"llm": llm, "verbose":True, "response_parser": StreamlitResponse})
+                st.sidebar.success("Chat agent initialized successfully.")
+            else:
+                pass
+        
+        else:
+            st.sidebar.error("Currently, only OpenAI and Azure Open AI are supported.")
+
+tab1, tab2 = st.tabs(["**Data Explorer**", "**Chat with Data**"])
+with tab1:
+    st.markdown("### Data Explorer")
+    if df is not None:
         pyg_app = StreamlitRenderer(df)
         pyg_app.explorer()
+    else:
+        st.warning("Please upload a file first.")
+
+with tab2:
+    st.markdown("### Chat with Data")
+    if df is not None:
+        if llm_provider in agent_provider_options:
+            if llm_key is not None and agent is not None:
+                st.markdown(f"#### {llm_provider} Chat")
+                user_input = st.chat_input(placeholder="Ask a question")
+                with st.spinner("AI is thinking..."):
+                    response = agent.chat(user_input)
+                    st.write(response)
+            else:
+                st.error(f"Please enter your {llm_provider} API key.")
+        else:
+            st.error(f"Currently, only {agent_provider_options} is supported.")
+    else:
+        st.warning("Please upload a file first.")
+
+
 
